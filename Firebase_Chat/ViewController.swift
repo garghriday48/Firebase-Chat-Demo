f6afbe8abff4ec717fdc5f3e74bc9ca712b98d59
Hriday garg <hriday.garg@chicmic.co.in>
2023-08-24 11:50:54 +0530

    Initial Commit

---
+++ Firebase_Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase_Chat
+//
+//  Created by Pragath on 24/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
